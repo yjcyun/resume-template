d945721c232029fbe136c78832331501e17f79f9
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-10-11 19:52:40 -0400

    Render side sections from js and clean up markup

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // Display name and title on very top
 function displayName(data) {
   const top = document.getElementById('top');
-  top.innerHTML = '';
 
   top.innerHTML = `
     <h1>${data.name}</h1>
@@ -12,7 +11,6 @@ function displayName(data) {
 // Display work experience
 function displayWorkExperience(data) {
   const workExperience = document.getElementById('work-experience');
-  workExperience.innerHTML = '';
 
   data.workExperiences.forEach(job => {
     const { title, company, site, period, desc } = job;
@@ -38,7 +36,6 @@ function displayWorkExperience(data) {
 // Display projects
 function displayProjects(data) {
   const projectsList = document.getElementById('projects');
-  projectsList.innerHTML = '';
 
   data.projects.forEach(project => {
     const { title, desc, site, stacks } = project;
@@ -58,12 +55,60 @@ function displayLink(link) {
   return link.replace('https://', '')
 }
 
+// Contact section
+function displayContact(data) {
+  const contact = document.getElementById('contact');
+  data.personalInfo.forEach(info => {
+    const { title, desc, link1, link2 } = info;
+    contact.innerHTML += `
+      ${title === 'Websites'
+        ? `<div class="contact-section">
+            <h5>${title}</h5>
+            <span><a href=${link1}>${displayLink(link1)}</a></span><br/>
+            <span><a href=${link2}>${displayLink(link2)}</a></span>
+          </div>`
+        : `<div class="contact-section">
+            <h5>${title}</h5>
+            <p>${desc}</p>
+          </div>`
+      }  
+    `
+  })
+}
 
+// Display skills/ rograms
+function displaySkills(data) {
+  const programs = document.getElementById('programs');
+  data.skills.forEach(skill => {
+    programs.innerHTML += `
+      <li>${skill}</li>
+    `
+  })
+}
 
+// Display education
+function displayEducation(data) {
+  const education = document.getElementById('education');
+  data.educations.forEach(edu => {
+    const { degree, school, locationYear } = edu;
+
+    education.innerHTML += `
+      <div class="contact-section">
+        <h5>${degree}</h5>
+        <p>${school}</br>${locationYear}</p>
+      </div>
+    `
+  })
+}
+
+// Fetch data
 fetch('./resume.json')
   .then(response => response.json())
   .then(data => {
     displayName(data);
     displayWorkExperience(data);
     displayProjects(data);
+    displayContact(data);
+    displaySkills(data);
+    displayEducation(data);
   });
